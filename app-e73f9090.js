ca6905cc053de6708e3fd1b3db0b982e54da72e1
carlyleec <carlyleec@gmail.com>
2015-06-06 16:51:52 -0400

    last minute changes

--- app-e73f9090.js
+++ app-e73f9090.js
@@ -29,7 +29,7 @@ rentapply.config(["$stateProvider", "$urlRouterProvider", "$httpProvider", "$com
         controller: 'RegisterToFindTenantsCtrl'
       })
       .state('find', {
-        url: '/find/:username',
+        url: '/find',
         templateUrl: 'find/find.html',
         controller: 'FindCtrl'
       })
@@ -61,7 +61,20 @@ rentapply.controller('ApplyCtrl', ['$scope', '$state', '$stateParams', '$locatio
 }]);
 rentapply.controller('FindCtrl', ['$scope', '$state', '$stateParams', '$location', '$http', '$timeout',
   function ($scope, $state, $stateParams, $location, $http, $timeout) {
+var ref = new Firebase("https://openhousing.firebaseio.com/applications");
 
+
+
+
+
+    ref.on("value", function(snapshot) {
+      console.log(snapshot.val());
+      $scope.applications = snapshot.val();
+
+    }, function (errorObject) {
+      console.log("The read failed: " + errorObject.code);
+    });
+    
 
 
 }]);
@@ -138,8 +151,6 @@ rentapply.controller('ProfileCtrl', ['$scope', '$state', '$stateParams', '$locat
 
     var ref = new Firebase("https://openhousing.firebaseio.com/users");
 
-    //$scope.users = $firebaseArray(ref);
-    console.log($scope.users);
 
     $scope.user = {};
 
@@ -148,10 +159,11 @@ rentapply.controller('ProfileCtrl', ['$scope', '$state', '$stateParams', '$locat
     };
 
     $scope.register = function(){
+        $scope.user.registered = true;
+        console.log($scope.user);
         $('#registerModal').modal('hide');
     }
 
-    console.log($stateParams.username);
 
     
 
@@ -162,10 +174,19 @@ rentapply.controller('ProfileCtrl', ['$scope', '$state', '$stateParams', '$locat
         }
     };
 
-    
-    
 
-    console.log($scope.users);
+
+    ref.on("value", function(snapshot) {
+      var users = snapshot.val();
+      for(key in users){
+        if(users[key].userName == $stateParams.username){
+            $scope.user = users[key];
+        }
+      }
+    }, function (errorObject) {
+      console.log("The read failed: " + errorObject.code);
+    });
+    
 
 }]);
 rentapply.controller('RegisterToFindTenantsCtrl', ['$scope', '$stateParams', '$state', '$timeout',
@@ -182,8 +203,8 @@ try {
   module = angular.module('rentapply', []);
 }
 module.run(['$templateCache', function($templateCache) {
-  $templateCache.put('apply/apply.html',
-    '<div class="col-xs-12"><div class="col-xs-12" ng-controller="ApplyCtrl as apply"><div class="jumbotron"><h1>Apply for Housing</h1><p>Description here of what to expect in this page ...</p></div><form><div class="form-group col-xs-6"><label for="applicant_firstname">First Name</label> <input type="text" class="form-control" ng-model="application.firstName" id="applicant_firstname" placeholder="First Name"></div><div class="form-group col-xs-6"><label for="applicant_lastname">Last Name</label> <input type="text" class="form-control" ng-model="application.lastName" id="applicant_lastname" placeholder="Last Name"></div><div class="form-group col-xs-4"><label for="applicant_SSN">SSN</label> <input type="number" class="form-control" ng-model="application.ssn" id="applicant_SSN" placeholder="Enter social security #"></div><div class="form-group col-xs-4"><label for="applicant_drivers_license">Drivers License</label> <input type="number" class="form-control" ng-model="application.driversLicense" id="applicant_drivers_license" placeholder="Enter Drivers License #"></div><div class="form-group col-xs-4"><label for="applicant_phone">Phone Number</label> <input type="phone" class="form-control" ng-model="application.phone" id="applicant_phone" placeholder="Enter Phone Number #"></div><div class="form-group col-xs-12"><label for="applicant_street">Street Current Address</label> <input type="number" class="form-control" ng-model="application.address.street" id="applicant_street" placeholder="Enter Street Address"></div><div class="form-group col-xs-6"><label for="applicant_city">City</label> <input type="number" class="form-control" ng-model="application.address.city" id="applicant_city" placeholder="Enter City"></div><div class="form-group col-xs-2"><label for="applicant_state">State</label> <input type="number" class="form-control" ng-model="application.address.state" id="applicant_state" placeholder="Enter State"></div><div class="form-group col-xs-4"><label for="applicant_postal_code">ZIP Code</label> <input type="number" class="form-control" ng-model="application.address.postal_code" id="applicant_postal_code" placeholder="Postal (ZIP)#"></div><div class="form-group col-xs-4"><label for="applicant_type">Currently Own or Rent</label><div class="radio"><label><input type="radio" name="appType" id="appTypeRent" value="rent" checked=""> Rent</label></div><div class="radio"><label><input type="radio" name="appType" id="appTypeOwn" value="own"> Own</label></div></div><div class="form-group col-xs-12"><button class="btn btn-default" ng-click="sendApplication()">Submit</button> <button class="btn btn-default" ng-click="clearApplication()">Clear</button> <button class="btn btn-warning" ui-sref="home">Home</button></div></form></div></div>');
+  $templateCache.put('find/find.html',
+    '<div class="col-xs-12 col-md-8 col-md-offset-2"><table class="table table-striped"><table class="table table-striped"><thead><tr><th>First Name</th><th>Last Name</th></tr></thead><tbody><tr ng-repeat="applicant in applications"><td>{{applicant.firstName}}</td><td>{{applicant.lastName}}</td></tr></tbody></table></table></div>');
 }]);
 })();
 
@@ -194,8 +215,8 @@ try {
   module = angular.module('rentapply', []);
 }
 module.run(['$templateCache', function($templateCache) {
-  $templateCache.put('find/find.html',
-    '<div><h1>Find form</h1></div>');
+  $templateCache.put('apply/apply.html',
+    '<div class="col-xs-12"><div class="col-xs-12" ng-controller="ApplyCtrl as apply"><div class="jumbotron"><h1>Apply for Housing</h1><p>Description here of what to expect in this page ...</p></div><form><div class="form-group col-xs-6"><label for="applicant_firstname">First Name</label> <input type="text" class="form-control" ng-model="application.firstName" id="applicant_firstname" placeholder="First Name"></div><div class="form-group col-xs-6"><label for="applicant_lastname">Last Name</label> <input type="text" class="form-control" ng-model="application.lastName" id="applicant_lastname" placeholder="Last Name"></div><div class="form-group col-xs-4"><label for="applicant_SSN">SSN</label> <input type="number" class="form-control" ng-model="application.ssn" id="applicant_SSN" placeholder="Enter social security #"></div><div class="form-group col-xs-4"><label for="applicant_drivers_license">Drivers License</label> <input type="number" class="form-control" ng-model="application.driversLicense" id="applicant_drivers_license" placeholder="Enter Drivers License #"></div><div class="form-group col-xs-4"><label for="applicant_phone">Phone Number</label> <input type="phone" class="form-control" ng-model="application.phone" id="applicant_phone" placeholder="Enter Phone Number #"></div><div class="form-group col-xs-12"><label for="applicant_street">Street Current Address</label> <input type="number" class="form-control" ng-model="application.address.street" id="applicant_street" placeholder="Enter Street Address"></div><div class="form-group col-xs-6"><label for="applicant_city">City</label> <input type="number" class="form-control" ng-model="application.address.city" id="applicant_city" placeholder="Enter City"></div><div class="form-group col-xs-2"><label for="applicant_state">State</label> <input type="number" class="form-control" ng-model="application.address.state" id="applicant_state" placeholder="Enter State"></div><div class="form-group col-xs-4"><label for="applicant_postal_code">ZIP Code</label> <input type="number" class="form-control" ng-model="application.address.postal_code" id="applicant_postal_code" placeholder="Postal (ZIP)#"></div><div class="form-group col-xs-4"><label for="applicant_type">Currently Own or Rent</label><div class="radio"><label><input type="radio" name="appType" id="appTypeRent" value="rent" checked=""> Rent</label></div><div class="radio"><label><input type="radio" name="appType" id="appTypeOwn" value="own"> Own</label></div></div><div class="form-group col-xs-12"><button class="btn btn-default" ng-click="sendApplication()">Submit</button> <button class="btn btn-default" ng-click="clearApplication()">Clear</button> <button class="btn btn-warning" ui-sref="home">Home</button></div></form></div></div>');
 }]);
 })();
 
@@ -219,7 +240,7 @@ try {
 }
 module.run(['$templateCache', function($templateCache) {
   $templateCache.put('profile/profile.html',
-    '<div class="col-xs-12 col-md-6 col-md-offset-3"><h1 class="text-center">Welcome {{user.firstname}} {{user.lastname}}</h1><div class="col-xs-12"><a href="#/apply" class="btn btn-primary btn-lg col-xs-12" style="margin-top : 20px">Submit a Rental Application</a> <a ng-click="registerModal()" class="btn btn-primary btn-lg col-xs-12" style="margin-top : 20px">Register to find Tenants</a></div><div id="registerModal" class="modal fade"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><h4 class="modal-title">Register to find Tenants</h4></div><div class="modal-body"><form><div class="checkbox"><label><input type="checkbox"> I want to register</label></div></form></div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button> <button type="button" class="btn btn-primary" ng-click="register()">Register</button></div></div></div></div></div>');
+    '<div class="col-xs-12 col-md-6 col-md-offset-3"><h1 class="text-center">Welcome {{user.firstName}} {{user.lastName}}</h1><div class="col-xs-12"><a href="#/apply" class="btn btn-primary btn-lg col-xs-12" style="margin-top : 20px">Submit a Rental Application</a> <a ng-if="!user.registered" ng-click="registerModal()" class="btn btn-primary btn-lg col-xs-12" style="margin-top : 20px">Register to find Tenants</a> <a ng-if="user.registered" href="#/find" class="btn btn-primary btn-lg col-xs-12" style="margin-top : 20px">Find Tenants</a></div><div id="registerModal" class="modal fade"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><h4 class="modal-title">Register to find Tenants</h4></div><div class="modal-body"><form><div class="checkbox"><label><input type="checkbox"> I want to register</label></div></form></div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button> <button type="button" class="btn btn-primary" ng-click="register()">Register</button></div></div></div></div></div>');
 }]);
 })();
 
